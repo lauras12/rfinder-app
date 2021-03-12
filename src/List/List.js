fb09ce26de1dd5139b1cef93266a6240d9dc8b1d
Laura Sikora <lauraannsikora@gmail.com>
2021-03-12 11:01:51 -0400

    updated search and api call

--- src/List/List.js
+++ src/List/List.js
@@ -1,12 +1,32 @@
 import React from 'react';
-
+import RestaurantContext from '../Context';
+import ListItem from '../ListItem/ListItem';
 
 export default class List extends React.Component {
+    static contextType = RestaurantContext;
     render() {
-        return (
-            <div>
-                List
-            </div>
+        let {list} = this.context;
+        list = list.map(item => {
+                return (
+                   <ListItem 
+                    key={item.id}
+                    id={item.id}
+                    name={item.name}
+                    location={item.location}
+                    phone={item.phone}
+                    price={item.price}
+                    img={item.image_url}
+                    website={item.url}
+                    rating={item.rating}
+                   />
+                )
+            })
+         
+        return(
+            <ul>
+                {list}
+            </ul>
         )
+        
     }
-} 
+}
--- src/SmallForm/SmallForm.js
+++ src/SmallForm/SmallForm.js
@@ -1,19 +1,69 @@
 import React from 'react';
-import StatesSelect from './StatesSelect';
+import ProxyCalls from '../Services/ProxyCalls'
+import RestaurantContext from '../Context';
 
 export default class SmallForm extends React.Component {
+    static contextType = RestaurantContext;
+constructor(){
+    super()
+    this.state = {
+        error: null,
+        stateSelection: null,
+        city: null,
+        term: null,
+    }
+}
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        const location = this.state.city;
+        const term = 'coffee';
+        
+        ProxyCalls.getFromRestaurantFinderApi(term, location)
+        .then(data => {
+            console.log(this.context)
+            this.context.setList(data.businesses)
+        })
+        .catch(err => {
+            this.setState({
+                error: err
+            })
+        })
+    }
+    
+    handleCityInput = (e)=> {
+        e.preventDefault();
+        const location = e.target.value;
+        this.setState({
+            city: location,
+        })
+    }
+    handleTypeInput = (e)=> {
+        e.preventDefault();
+        console.log(e.target.value)
+        this.setState({
+            term: e.target.value,
+        })
+    }
+
     render(){
         return (
             <div>
-                <form>
-                <h2>Search restaurants in : </h2>
+                <form onSubmit={this.handleFormSubmit}>
+                <h2>Search for: </h2>
+                <select onChange={this.handleTypeInput} required>
+                    <option value="coffee">Coffee-shops</option>
+                    <option value="juice">Juice-Bars</option>
+                    <option value="food">Restaurants</option>
+                   
+                </select>
+                <h2>in :</h2>
                 <label>City</label>
-                <input type='text' />
-                <StatesSelect /> 
+                <input type='text' onChange={this.handleCityInput} required/>
                <br />
                 <button>Search</button>
             </form>
+        
             </div>
         )
     }
-} 
+}
