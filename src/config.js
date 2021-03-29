88db5541f879deaa8ac7674c39520ebcb24e13c8
Laura Sikora <lauraannsikora@gmail.com>
2021-03-29 14:05:02 -0400

    updated config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
-    API_ENDPOINT: 'https://rfinder-api.herokuapp.com/',
+    API_ENDPOINT: (process.env.NODE_ENV === 'production' ? 'https://rfinder-api.herokuapp.com/api' : 'http://localhost:8000/api'),
     API_KEY: process.env.REACT_API_KEY,
 }
