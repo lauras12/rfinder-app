d805e267d22ce2f6148285e2723977ae16d6c1c9
Laura Sikora <lauraannsikora@gmail.com>
2021-04-02 09:34:49 -0400

    updated api endpoint for deploy

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@
 }
 */
 export default {
-    API_ENDPOINT: 'http://localhost:8000/api',
+    //API_ENDPOINT: 'http://localhost:8000/api',
+    API_ENDPOINT:'https://rfinder-api.herokuapp.com/api',
     REACT_APP_API_KEY: process.env.REACT_APP_API_KEY,
 }
